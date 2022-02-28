9e01ddddd6d25d8f34fa126bd18261f715c3ffd9
Samuelodan <samodan195@gmail.com>
2022-02-28 17:36:33 +0100

    Pass tests up to 07_tempConversion

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
-};
+const leapYears = function (year){
+    if (year % 4 != 0){
+      return false;
+    } else if (year >= 400 && year % 400 != 0 &&
+               year % 100 == 0){
+      return false
+    }
+    return true;
+}
 
 // Do not edit below this line
 module.exports = leapYears;
